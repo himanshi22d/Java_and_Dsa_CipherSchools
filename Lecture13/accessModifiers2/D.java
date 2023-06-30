0619334425d27503f05345b74c4ddccd50ef016a
himanshi <108574343+himanshi22d@users.noreply.github.com>
2023-06-30 14:27:21 +0530

    D.java

---
+++ Lecture13/accessModifiers2/D.java
@@ -0,0 +1,13 @@
+package accessModifiers2;
+ 
+public class D
+{
+ public static void main(String[] args)
+     {
+         A obj = new A();
+ //      System.out.println(obj.x);
+ //      System.out.println(obj.y);
+         System.out.println(obj.z);
+ //      System.out.println(obj.w);
+     }
+}
